8f3ddacf7997634b3a9d152db71814ad8aaa966f
Nilsson1 <userfrenils@gmail.com>
2025-02-07 08:14:35 +0100

    clean up and moved some things around to logically appear nicer in maincontroller

--- DataAnalysisApp/Controllers/MainController.cs
+++ DataAnalysisApp/Controllers/MainController.cs
@@ -20,10 +20,9 @@ namespace DataAnalysisApp.Controllers
             dac = new DataAnalysisController();
         }
 
-        public void Run() //REMOVE AND JUST USE THE CONTROLLER ?
+        public void Run()
         {
             CalculateFalsePositivesIncorrectPasswords(dlc.GetLoginData());
-            //CalculateIncorrectPasswords(dlc.GetLoginData());
         }
 
         private void CalculateFalsePositivesIncorrectPasswords(List<LoginData> loginData)
@@ -34,7 +33,8 @@ namespace DataAnalysisApp.Controllers
             // Iterate each Login attempt and calculate their scores based on individual components of the Login.
             foreach (var attempt in attempts)
             {
-                Console.WriteLine($"***** Analyzing: {attempt.Timestamp}, {attempt.UserID}, {attempt.IPAddress}, {attempt.DeviceName}, {attempt.OperatingSystem}, {attempt.Location} *****");
+                Console.WriteLine($"***** Analyzing: {attempt.Timestamp}, {attempt.UserID}, {attempt.IPAddress}, {attempt.DeviceName}, " +
+                    $"{attempt.OperatingSystem}, {attempt.Location} *****");
                 var userHistory = dlc.GetUserHistoryData(attempt.UserID); //Get history for analysis
                 double timestampScore = Calc_TimeStamp(attempt, userHistory);
                 double osScore = Calc_OS(attempt, userHistory);
@@ -52,14 +52,59 @@ namespace DataAnalysisApp.Controllers
             var sortedResultsByScore = results.OrderByDescending(r => r.Score).ToList();
             foreach (var result in sortedResultsByScore)
             {
-                Console.WriteLine($"{String.Format("{0:0.00}", result.Score)} : {result.LoginAttempt.Timestamp}, {result.LoginAttempt.UserID}, {result.LoginAttempt.IPAddress}, {result.LoginAttempt.DeviceName}, {result.LoginAttempt.OperatingSystem}, {result.LoginAttempt.Location}");
+                Console.WriteLine($"{String.Format("{0:0.00}", result.Score)} : {result.LoginAttempt.Timestamp}, {result.LoginAttempt.UserID}, " +
+                    $"{result.LoginAttempt.IPAddress}, {result.LoginAttempt.DeviceName}, {result.LoginAttempt.OperatingSystem}, " +
+                    $"{result.LoginAttempt.Location}");
             }
+        }
+
+        private double Calc_TimeStamp(LoginData loginAttempt, List<LoginData> userHistory)
+        {
+            Console.WriteLine("** Analyzing TimeStamp **");
+
+            //Convert timestamp to TimeOfDay for analysis, don't care about which day login attempt occurred only the time of day.
+            List<TimeSpan> loginTimesOfDay = userHistory.Select(login => login.Timestamp.TimeOfDay).ToList();
+
+            // Calculate mean
+            TimeSpan mean = new TimeSpan(Convert.ToInt64(loginTimesOfDay.Average(ts => ts.Ticks)));
+            Console.WriteLine($"Mean: {mean}");
+
+            // Calculate standard deviation
+            TimeSpan stdDev = dac.GetStandardDeviation(loginTimesOfDay);
+            Console.WriteLine($"Standard Deviation: {stdDev}");
+
+            // Calculate 95% confidence interval
+            var confidenceInterval = dac.GetConfidenceInterval(loginTimesOfDay);
+            Console.WriteLine($"95% Confidence Interval: ({confidenceInterval.Item1}, {confidenceInterval.Item2})");
+
+            var zScore = dac.GetZScore(loginAttempt.Timestamp.TimeOfDay, mean, stdDev);
+            Console.WriteLine($"Login Time: {loginAttempt.Timestamp.TimeOfDay}, Z-Score: {zScore:F2}");
+            return Math.Abs(zScore);
         }
 
         private double OverallScoreCalculator(double os, double timeStamp, double ipAddress, double deviceName, double location)
         {
             double score = os + timeStamp + ipAddress + deviceName + location;
             return score;
+        }
+
+        private double Calc_OS(LoginData loginAttempt, List<LoginData> userHistory)
+        {
+            Console.WriteLine("** Analyzing OS **");
+
+            double result = 0.0;
+            var osMatches = userHistory.Where(user => user.OperatingSystem == loginAttempt.OperatingSystem).ToList();
+            foreach (var attempt in osMatches)
+            {
+                //Console.WriteLine($"{attempt.Timestamp}: {attempt.UserID} - {attempt.IPAddress} ({attempt.DeviceName}, {attempt.OperatingSystem}, {attempt.Location})");
+            }
+            var osPercent = Math.Round((double)osMatches.Count / userHistory.Count, 2);
+            Console.WriteLine(osPercent + " : " + osMatches.Count + "/" + userHistory.Count);
+            if (osPercent > 0.5)
+            {
+                return result = 0.5;
+            }
+            return result;
         }
 
         private double Calc_Location(LoginData loginAttempt, List<LoginData> userHistory) 
@@ -86,60 +131,6 @@ namespace DataAnalysisApp.Controllers
 
             double result = 0.0;
             return result;
-        }
-
-        private double Calc_OS(LoginData loginAttempt,  List<LoginData> userHistory)
-        {
-            Console.WriteLine("** Analyzing OS **");
-
-            double result = 0.0;
-            var osMatches = userHistory.Where(user => user.OperatingSystem == loginAttempt.OperatingSystem).ToList();
-            foreach (var attempt in osMatches)
-            {
-                //Console.WriteLine($"{attempt.Timestamp}: {attempt.UserID} - {attempt.IPAddress} ({attempt.DeviceName}, {attempt.OperatingSystem}, {attempt.Location})");
-            }
-            var osPercent = Math.Round((double)osMatches.Count / userHistory.Count, 2);
-            Console.WriteLine(osPercent + " : " + osMatches.Count + "/" + userHistory.Count);
-            if (osPercent > 0.5)
-            {
-                return result = 0.5;
-            }
-            return result;
-        }
-
-        private double Calc_TimeStamp(LoginData loginAttempt, List<LoginData> userHistory)
-        {
-            Console.WriteLine("** Analyzing TimeStamp **");
-
-            //Convert timestamp to TimeOfDay for analysis, don't care about which day login attempt occurred only the time of day.
-            List<TimeSpan> loginTimesOfDay = userHistory.Select(login => login.Timestamp.TimeOfDay).ToList();
-
-            // Calculate mean
-            TimeSpan mean = new TimeSpan(Convert.ToInt64(loginTimesOfDay.Average(ts => ts.Ticks)));
-            Console.WriteLine($"Mean: {mean}");
-
-            // Calculate standard deviation
-            TimeSpan stdDev = dac.GetStandardDeviation(loginTimesOfDay);
-            Console.WriteLine($"Standard Deviation: {stdDev}");
-
-            // Calculate 95% confidence interval
-            var confidenceInterval = dac.GetConfidenceInterval(loginTimesOfDay);
-            Console.WriteLine($"95% Confidence Interval: ({confidenceInterval.Item1}, {confidenceInterval.Item2})");
-
-            var zScore = dac.GetZScore(loginAttempt.Timestamp.TimeOfDay, mean, stdDev);
-            Console.WriteLine($"Login Time: {loginAttempt.Timestamp.TimeOfDay}, Z-Score: {zScore:F2}");
-            return Math.Abs(zScore); //Positive number for the further calculation of scores, higher score = bad, and it doesn't matter whether you're are way off LOWER or UPPER bound.
-        }
-
-        private void CalculateIncorrectPasswords(List<LoginData> loginData) //OLD
-        {
-            var falsePositives = loginData.Where(r => r.Result == "Fail" && r.Reason == "Incorrect Password").ToList();
-
-            // Output false positives to console
-            foreach (var attempt in falsePositives)
-            {
-                Console.WriteLine($"{attempt.Timestamp}: {attempt.UserID} - {attempt.IPAddress} ({attempt.DeviceName}, {attempt.OperatingSystem}, {attempt.Location})");
-            }
         }
     }
 }
